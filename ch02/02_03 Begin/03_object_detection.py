0f9c9bff7f751f68dc44bc13a215ef802bb5bc76
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-06 20:37:17 -0500

    1.finding binary with adaptive threshold

--- ch02/02_03 Begin/03_object_detection.py
+++ ch02/02_03 Begin/03_object_detection.py
@@ -2,6 +2,19 @@ import numpy as np
 import cv2
 
 img = cv2.imread("fuzzy.png",1)
+cv2.imshow("Original",img)
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (3,3),0)
+
+thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,205,1)
+
+cv2.imshow("Binary",thresh)
+
+
+
+
 
 
 cv2.waitKey(0)
