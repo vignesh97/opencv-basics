edb65fb415b1e97eaf0bebe9b1c0ff1c79a40d86
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-06 22:06:15 -0500

    face-detection and eye detection

--- ch02/02_03 Begin/eye-detection.py
+++ ch02/02_03 Begin/eye-detection.py
@@ -11,11 +11,19 @@ eye_cascade = cv2.CascadeClassifier(path)
 
 
 
-eyes = eye_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors=20, minSize = (10,10))
+eyes = eye_cascade.detectMultiScale(gray, scaleFactor = 1.07, minNeighbors=20, minSize = (10,10))
 print(len(eyes))
 
 for(x,y,w,h) in eyes:
-	cv2.rectangle(img, (x,y), (x+w,y+h),(0,255,0),2)
+	xc = (x + x+w)/2
+	yc = (y + y+h)/2
+	radius = w/2
+	cv2.circle(img,(int(xc),int(yc)),int(radius),(255,0,0),2)
+
+
+	#cv2.rectangle(img, (x,y), (x+w,y+h),(0,255,0),2)
+
+
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
