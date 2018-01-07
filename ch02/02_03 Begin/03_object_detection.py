fc71aafbc84a22224dc42d32cc5c58916ea2448a
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-06 20:54:46 -0500

    2.detecting contours, 3.random color

--- ch02/02_03 Begin/03_object_detection.py
+++ ch02/02_03 Begin/03_object_detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import random
 
 img = cv2.imread("fuzzy.png",1)
 cv2.imshow("Original",img)
@@ -14,7 +15,27 @@ cv2.imshow("Binary",thresh)
 
 
 
+_,contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+print(len(contours))
+
+filtered = []
+for c in contours: 
+	if cv2.contourArea(c) < 1000:continue
+	filtered.append(c)
+
+print(len(filtered))
+
+objects = np.zeros([img.shape[0], img.shape[1],3], 'uint8')
+for c in filtered:
+	col = (random.randint(0,255),random.randint(0,255), random.randint(0,255))
+	cv2.drawContours(objects,[c],-1,col,-1)
+	area = cv2.contourArea(c)
+	p = cv2.arcLength(c,True)
+	print(area,p)
+
+
+cv2.imshow("Contours", objects)
 
 
 cv2.waitKey(0)
